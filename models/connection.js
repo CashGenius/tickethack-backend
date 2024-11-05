e94b06e84f8c29d6cedd16ab4b46ddd9d764f095
Thomas Lebel <thomas.lebel38@gmail.com>
2024-11-05 18:35:31 +0100

    Modification de la connection string

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://admin:IJfElssjlAhcDysk@cluster0.p6urc.mongodb.net/tickethack';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
  .then(() => console.log('Database connected'))
