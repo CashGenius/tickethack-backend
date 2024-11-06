8608e5a2072f26fe3ccaada90ab5f7980e4f13c5
Thomas Lebel <thomas.lebel38@gmail.com>
2024-11-06 13:55:37 +0100

    Ajout de route pour récupérer les documents de la BDD bookings

--- routes/bookings.js
+++ routes/bookings.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+const Booking = require('../models/bookings')
+
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/displayBookings', function(req,res){
+  Booking.find({})
+  .then(data =>{
+      console.log(data)
+      res.json({result: true, items: data})
+  })
+})
+
 
 module.exports = router;
--- routes/trips.js
+++ routes/trips.js
@@ -17,7 +17,7 @@ router.get('/displayAllTrips', function(req, res, next) {
 router.get('/findTrips', function(req, res, next) {
     const departure = req.body.departure
     const arrival = req.body.arrival
-    let date = req.body.date
+    let date = moment(req.body.date).format('DD-MM-YYYY')
     if (date !== "" && date !== undefined && date !== null){
         date = moment(req.body.date,'DD-MM-YYYY').format('L')
     } else {
